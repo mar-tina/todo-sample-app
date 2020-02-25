c7e126c0a14081aec313e99d7aef025764a3aff5
mar-tina <martinakhaemba@gmail.com>
2020-02-25 17:40:07 +0300

    set maximum scroll height

--- src/components/todolist.js
+++ src/components/todolist.js
@@ -1,5 +1,5 @@
 import { html, createShadowElement } from "shadow-render";
-import { todoItems, todoItem } from "./appStyle.js";
+import { todoItems, todoItem, todoListHolder } from "./appStyle.js";
 import { sanitize } from "./utils.js";
 
 let TodoList = createShadowElement({
@@ -63,35 +63,40 @@ let TodoList = createShadowElement({
   },
   template: ctx => {
     return html(`
-            <div style="text-align:center;"> All the todos </div>
+    <div style="text-align:center;"> All the todos </div>
+    <div style="${todoListHolder}">
             <div style="${todoItems}" id="todoList"> ${todoList(
       ctx.state
     )} </div>
+    </div>
         `);
   }
 });
 
 let todoList = state => {
-  return `${state.todos
-    .map(
-      x =>
-        ` <div @oninput="handleEdit" contentEditable=${true} style="${todoItem}" id=${x.id +
-          "none"}  bind=${x.id}>  ${x.name} </div> 
-            <div style="text-align:center;"> 
-            <!-- Append a randomized string to svg ID to avoid more than one element sharing the same id -->
-            <svg id=${x.id + "rand"} @onclick="deleteTodo"  bind=${
-          x.id
-        } xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+  return `
+    ${state.todos
+      .map(
+        x =>
+          ` 
+          <div @oninput="handleEdit" contentEditable=${true} style="${todoItem}" id=${x.id +
+            "none"}  bind=${x.id}>  ${x.name} </div> 
+              <div style="text-align:center;"> 
+              <!-- Append a randomized string to svg ID to avoid more than one element sharing the same id -->
+              <svg id=${x.id + "rand"} @onclick="deleteTodo"  bind=${
+            x.id
+          } xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
 
-            <!-- Append a randomized string to svg ID to avoid more than one element sharing the same id -->
-            <svg id=${x.id + "abs"} @onclick="markAsDone" bind=${
-          x.id
-        }  xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke=${
-          x.done ? "green" : "black"
-        } stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>
-        </div>`
-    )
-    .join("")}`;
+              <!-- Append a randomized string to svg ID to avoid more than one element sharing the same id -->
+              <svg id=${x.id + "abs"} @onclick="markAsDone" bind=${
+            x.id
+          }  xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke=${
+            x.done ? "green" : "black"
+          } stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>
+          </div>
+      `
+      )
+      .join("")}`;
 };
 
 customElements.define("todo-list", TodoList);
