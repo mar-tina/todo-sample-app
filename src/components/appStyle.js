9bee169df3c6d297a72e8f3e2f081d04df57e70d
mar-tina <martinakhaemba@gmail.com>
2020-02-25 17:39:40 +0300

    modified style for logger element

--- src/components/appStyle.js
+++ src/components/appStyle.js
@@ -33,3 +33,34 @@ export let todoItems = `
     display: grid;
     grid-gap: 15px;
 `;
+
+export let logTodoContainer = `
+    display: grid;
+    grid-gap: 100px;
+    grid-template-columns: 1fr 1fr;
+`;
+
+export let logHolder = `
+    display:grid;
+    justify-items:center;
+    align-content: center;
+    grid-gap: 20px;
+    overflow-y: scroll;
+    max-height: 400px;
+`;
+
+export let headerItem = `
+    display: grid;
+    border-bottom: 1px solid #FF5733;
+    padding-bottom: 10px;
+    color: #FF5733;
+    text-align: center;
+`;
+
+export let todoListHolder = `
+    justify-items:center;
+    align-content: center;
+    grid-gap: 20px;
+    overflow-y: scroll;
+    max-height: 400px;
+`;
